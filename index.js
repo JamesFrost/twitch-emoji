20df1410a080e30454f0a694f5a8e303d868f749
James Frost <james.frost@blurrt.co.uk>
2016-09-12 17:31:55 +0100

    Added content

---
+++ index.js
@@ -0,0 +1,31 @@
+const _global = require( './json/v2/global.json' );
+const _globalSearchTerms = Object.keys( _global.emotes );
+
+exports.parse = function( text, emojiSize )
+{
+	const matchedEmojis = _searchString( _globalSearchTerms, text );
+
+	matchedEmojis.map(function( thisMatchedEmoji )
+	{
+		const imageId = _global.emotes[ thisMatchedEmoji ].image_id;
+		const imageUrl = _global.template[ emojiSize ].replace( '{image_id}', imageId );
+
+		text = text.replace( new RegExp( '\\b' + thisMatchedEmoji + '\\b', 'g' ), '<img src="' + imageUrl + '"/>' );
+	});
+
+	return text;
+};
+
+
+const _searchString = function( terms, string )
+{
+	if( terms.length === 0 )
+		return [];
+
+	const thisTerm = terms.slice( 0, 1 );
+
+	if( string.match( new RegExp( '\\b' + thisTerm + '\\b' ) ) )
+		return _searchString( terms.slice( 1 ), string ).concat( thisTerm );
+
+	return _searchString( terms.slice( 1 ), string );
+}
