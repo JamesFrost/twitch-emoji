40283cf30c181845f5b593ac44f96ebaf1d32799
James Frost <james.frost@blurrt.co.uk>
2016-10-20 17:23:01 +0100

    Updated github page

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,7 @@
 $( document ).ready(function()
 {
+	$( '#output' ).html( twitchEmoji.parse( $( '#demo-input' ).val(), { emojiSize : 'large' } ) );
+
 	$( '#demo-input' ).bind('input', function()
 	{
 		$( '#output' ).html( twitchEmoji.parse( $( this ).val(), { emojiSize : 'large' } ) );
