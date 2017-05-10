edadbf428afa0f5c6c7e3f0b82b8ec0b4db687d8
James Frost <james.frost@blurrt.co.uk>
2017-05-10 15:07:31 +0100

    Localhost TravisCI file server

---
+++ scripts/setup-local-file-server.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+npm install -g --silent http-server
+
+CURRENT_VERSION=$(npm run version --silent)
+
+echo Current Twitch Emoji Verison: $CURRENT_VERSION
+
+echo Copying files...
+mkdir twitch-emoji@$CURRENT_VERSION
+cp -R dist twitch-emoji@$CURRENT_VERSION
+
+echo Starting server...
+http-server -o &
